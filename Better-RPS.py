8806fe8b28a6bd779659fbbc2df6b27e6e06bf71
JVN313 <jvncovington@gamil.com>
2021-09-13 13:58:22 -0400

    added Better RPS

--- Better-RPS.py
+++ Better-RPS.py
@@ -10,21 +10,27 @@ user_input = input("Type Rock, Paper, Scissors, or Q to quit the game: ").upper(
 while playing:
     if user_input == "Q":
         playing = False
-    elif user_input == answer:
+    elif user_input == "PAPER" and answer == "ROCK":
         player_wins+=1
         print("You Won!")
         answer = options[random.randrange(0,3)]
         user_input = input("To play again type Rock, Paper, Scissors, or Q to quit the game: ").upper()
-    elif user_input != answer:
+    elif user_input == "ROCK" and answer == "SCISSORS":
+        player_wins+=1
+        print("You Won!")
+        answer = options[random.randrange(0,3)]
+        user_input = input("To play again type Rock, Paper, Scissors, or Q to quit the game: ").upper()
+    elif user_input == "SCISSORS" and answer == "PAPER":
+        player_wins+=1
+        print("You Won!")
+        answer = options[random.randrange(0,3)]
+        user_input = input("To play again type Rock, Paper, Scissors, or Q to quit the game: ").upper()
+    else:
         computer_wins+=1
         print("Sorry You Lost :( ")
         answer = options[random.randrange(0,3)]
         user_input = input("To play again type Rock, Paper, Scissors, or Q to quit the game: ").upper()
-    else:
-        user_input not in options
-        print("Invalid Input")
-        answer = options[random.randrange(0,3)]
-        user_input = input("To play again type Rock, Paper, Scissors, or Q to quit the game: ").upper()
+    
 
 
 print("You Won This "+str(player_wins)+" Many Times. And Lost This "+str(computer_wins)+" Many Times")
